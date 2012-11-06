e216a588e1904f4c04235bab00d4947311da44c9
Jon Peel <jonathan.peel@maplecroft.com>
2012-11-06 15:39:28 +0000

    add global charts variable

--- chartit/static/chartit/js/chartloader.js
+++ chartit/static/chartit/js/chartloader.js
@@ -1,8 +1,11 @@
 // jQuery function to create a chart for each of the HighCharts Chart Options
-// JSON object (_chartit_hco_array) passed to web page from the view. 
+// JSON object (_chartit_hco_array) passed to web page from the view.
+
+charts =[]; 
+
 $(document).ready(function() {
 	$.each(_chartit_hco_array, function(index, chartoptions) {
-		chart = new Highcharts.Chart(chartoptions);
+		charts.push(new Highcharts.Chart(chartoptions));
 	});
 });
 
